4adf0252ec964e87036b0821b58fca3842a17279
whatfield73 <whatfield@cbcames.org>
2012-07-21 00:17:54 -0500

    Updated for WP7 compatibility and to add edit function

--- database.js
+++ database.js
@@ -13,22 +13,23 @@ onFailure: "",
 onMake: "",
 onFind: "",
 onDel: "",
-onInsert: ""
+onInsert: "",
+onEdit: ""
 },
 insert: function(data) {
-var currentdata = window.localStorage[this.databasename];
+var currentdata = window.localStorage.getItem(this.databasename);
 if (currentdata == "ready" || currentdata == "") {
 currentdata = "";
 newdata = enyo.json.stringify(data);
-window.localStorage[this.databasename] = "[" + newdata + "]";
+window.localStorage.setItem(this.databasename, "[" + newdata + "]");
 }
 else {
-currentdata = enyo.json.parse(window.localStorage[this.databasename]);
+currentdata = enyo.json.parse(window.localStorage.getItem(this.databasename));
 
 currentdata.push(data);
-console.log(enyo.json.stringify(currentdata));
+//console.log(enyo.json.stringify(currentdata));
 
-window.localStorage[this.databasename] = enyo.json.stringify(currentdata);
+window.localStorage.setItem(this.databasename,enyo.json.stringify(currentdata));
 }
 //var id = '{"' + num + '" :' +  enyo.json.stringify(data) + "}";
 //var newdata = enyo.mixin(currentdata, data);  //enyo.json.parse(id)
@@ -36,22 +37,22 @@ window.localStorage[this.databasename] = enyo.json.stringify(currentdata);
 this.doInsert({response: "Inserted"});
 },
 remove: function(data) {
-var currentdata = enyo.json.parse(window.localStorage[this.databasename]);
+var currentdata = enyo.json.parse(window.localStorage.getItem(this.databasename));
 currentdata.splice(data,1);
-window.localStorage[this.databasename] = enyo.json.stringify(currentdata);
+window.localStorage.setItem(this.databasename, enyo.json.stringify(currentdata));
 this.doDel({response: "Deleted."});
 },
 find: function(data, key) {
 if (data == "" || data == undefined) {
-var currentdata = window.localStorage[this.databasename];
+var currentdata = window.localStorage.getItem(this.databasename);
 if (currentdata == "ready" || currentdata == "") {
 this.doFind({response: "No data."});
 }else {
-this.doFind({response:  enyo.json.parse(window.localStorage[this.databasename])});
+this.doFind({response:  enyo.json.parse(window.localStorage.getItem(this.databasename))});
 }
 }
 else {
-var currentdata = enyo.json.parse(window.localStorage[this.databasename]);
+var currentdata = enyo.json.parse(window.localStorage.getItem(this.databasename));
 for (k = 1; k < currentdata.length; ++k) { 
 var fd = [];
 var i =0;
@@ -72,21 +73,21 @@ this.doFind({"response": [{"data": returndata}, {"ids": fd}]});
 make: function(data) {
 //window.localStorage.bookmark="";
 try {
-if (window.localStorage[data]) {
-if (window.localStorage[data] == "ready") {
+if (window.localStorage.getItem(data)) {
+if (window.localStorage.getItem(data) == "ready") {
 this.databasename = data;
 this.doMake({response: "opened"});
 return true;
 }
 else {
-var currentdata = enyo.json.parse(window.localStorage[data]);
+var currentdata = enyo.json.parse(window.localStorage.getItem(data));
 this.databasename = data;
 this.doMake({response: "Opened"});
 }
 }
 else {
 
-window.localStorage[data] = "ready";
+window.localStorage.setItem(data,  "ready");
 this.databasename = data;
 this.doMake({response: "Created."});
 }
@@ -94,5 +95,11 @@ this.doMake({response: "Created."});
 catch(e) {
 this.doFailure({response: "Error setting up."});
 }
+},
+update: function(data, key) {
+var currentdata = enyo.json.parse(window.localStorage.getItem(this.databasename));
+currentdata.splice(key,1,data);
+window.localStorage.setItem(this.databasename, enyo.json.stringify(currentdata));
+this.doEdit({response: "Edited."});
 }
 });
